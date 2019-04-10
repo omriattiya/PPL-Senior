df46c60a618e0225a80162cc7cc832baedcac86a
shiraez <shiraez@marvell.com>
2019-04-10 22:41:47 +0300

    2 a and b

--- Assignment 1/Ex2.py
+++ Assignment 1/Ex2.py
@@ -5,7 +5,8 @@ def isInstancePPL(object1, classInfo):
         raise TypeError("classInfo is not of type 'type'")
     print type(object1)
     print classInfo
-    return type(object1) >= classInfo
+    # return type(object1) >= classInfo
+    return is_instance(type(object1), classInfo )
 
 
 def numInstancePPL(object1, classInfo):
@@ -16,29 +17,52 @@ def numInstancePPL(object1, classInfo):
     # if not isInstancePPL(object1, classInfo):
     #     return 0
     print "1!!!"
-    ans = 0
-    classOb = type(object1)
+    return num_instance(type(object1), classInfo, 0)
 
-    c = list(classOb.__bases__)
-    for base in c:
-        print base
-        print type(object1)
-        ans = ans + 1
-        if base == classInfo:
-            return ans
-        classOb = type(base.__bases__)
-        print  classOb
+
+
+def num_instance(Cob1, cls, num):
+    if Cob1 is cls:
+        return num
+    else:
+        c = list(Cob1.__bases__)
+        for base in c:
+            return num_instance(base, cls, num+1)
+
+
+def isSubclassPPL(cls, classInfo):
+
+    pass
+
+def is_instance(Cob1, cls):
+    if Cob1 is cls:
+        return True
+    if Cob1 is object:
+        False
+    else:
+        c = list(Cob1.__bases__)
+        for base in c:
+            return is_instance(base, cls)
+
+
+
 
 
 
 class A(object): pass
 class B(A): pass
 class C(B): pass
+class D(C): pass
 
-c = C()
-print isInstancePPL(c, A)
-# print numInstancePPL(c, A)
-print C.__bases__ == B
-print list(type(c).__bases__)[0] == B
-print list(type(list(type(c).__bases__)[0]).__bases__)
-print B
+c = A()
+# print isInstancePPL(c, A)
+print numInstancePPL(c, A)
+# print C.__bases__ == B
+# print list(type(c).__bases__)[0] == B
+# print list(type(list(type(c).__bases__)[0]).__bases__)
+# print B
+# print type(c)
+# print ((type(c).__bases__)[0].__bases__)[0].__bases__
+# print c is C
+
+print type(c).__bases__[0] is object
